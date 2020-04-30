bc90110dddd67f8be8dcd1113c817ed0a21cf56f
Kirill Kozyrev <kirill.kozyrev@karuna.group>
2020-04-30 18:38:59 +0300

    add index, new, show views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,23 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
   def new
   end
 
   def create
-    
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :starttime, :endtime, :organizeremail, :organizertelegram, :link)
   end
 end
